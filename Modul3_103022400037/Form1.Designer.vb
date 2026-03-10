08aec1b0c200e305cbf9eb368ca63e7c14f2d3e1
nyuuX <114668143+nyuuX@users.noreply.github.com>
2026-03-10 07:14:24 +0700

    Menambahkan tampilan GUI

--- Modul3_103022400037/Form1.Designer.vb
+++ Modul3_103022400037/Form1.Designer.vb
@@ -22,16 +22,120 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Label1 = New Label()
+        ComboBox1 = New ComboBox()
+        ComboBox2 = New ComboBox()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        Label4 = New Label()
         SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(38, 35)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(72, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Satuan Awal"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(38, 53)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 1
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(38, 132)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(121, 23)
+        ComboBox2.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(38, 114)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(74, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Satuan Akhir"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(225, 53)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(301, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(225, 132)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(301, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(593, 53)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 6
+        Button1.Text = "Convert"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(225, 35)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(60, 15)
+        Label3.TabIndex = 7
+        Label3.Text = "Nilai Awal"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(225, 114)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(62, 15)
+        Label4.TabIndex = 8
+        Label4.Text = "Nilai Akhir"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(ComboBox2)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label1)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 
 End Class
--- Modul3_103022400037/Form1.vb
+++ Modul3_103022400037/Form1.vb
@@ -2,4 +2,13 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
 End Class
